de39314acd897d4c54c97f35aaebb36781bc1325
melbarch <elbarchany@gmail.com>
2020-02-18 23:07:29 +0100

    first solution

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function convert(){
+  const text = document.getElementById('text').value;
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  context.font = 'italic 20pt Calibri';
+  context.textAlign = 'center';
+  context.fillStyle = 'blue'; // color string, hex value, or RGB value.
+  context.fillText(text, 250, 250); // text and position
+}
