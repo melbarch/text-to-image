4989f5107ac587f415c98ab5dcdf12f8623396eb
melbarch <elbarchany@gmail.com>
2020-02-20 23:48:17 +0100

    Support multiline
    That was interesting! I feel like more math is coming

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,19 @@
 "use strict";
 
-function convert(){
+function convert() {
   const text = document.getElementById('text').value;
-  if(!text) return;
+  if (!text) return;
   const lines = text.split('\n');
   const canvas = document.getElementById('myCanvas');
   const context = canvas.getContext('2d');
 
-  context.fillStyle = "#cbd5e0";
+  context.fillStyle = '#cbd5e0';
   context.fillRect(0, 0, canvas.width, canvas.height);
-  
+
   context.font = 'italic 20pt Calibri';
-  context.textBaseline = "middle";
+  context.textBaseline = 'middle';
   context.fillStyle = 'black';
-  context.fillText(lines[0], 10, 20);
-  context.fillText(lines[1], 10, 50);
+  for (var index = 0; index < lines.length; index++) {
+    context.fillText(lines[index], 10, 20+(index*30));
+  }
 }
