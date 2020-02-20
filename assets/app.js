416b5fd3273db577bd79b004035bb5ef475c0c2a
melbarch <elbarchany@gmail.com>
2020-02-20 23:39:47 +0100

    Support multiline [part 1]

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,17 @@
 
 function convert(){
   const text = document.getElementById('text').value;
-  var canvas = document.getElementById('myCanvas');
-  var context = canvas.getContext('2d');
+  if(!text) return;
+  const lines = text.split('\n');
+  const canvas = document.getElementById('myCanvas');
+  const context = canvas.getContext('2d');
+
+  context.fillStyle = "#cbd5e0";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  
   context.font = 'italic 20pt Calibri';
-  context.textAlign = 'center';
-  context.fillStyle = 'blue'; // color string, hex value, or RGB value.
-  context.fillText(text, 250, 250); // text and position
+  context.textBaseline = "middle";
+  context.fillStyle = 'black';
+  context.fillText(lines[0], 10, 20);
+  context.fillText(lines[1], 10, 50);
 }
