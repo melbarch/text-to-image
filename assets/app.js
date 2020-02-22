590ab465d3b553f457669037ae54d1c17d2351e4
melbarch <elbarchany@gmail.com>
2020-02-22 23:18:52 +0100

    Take text size into account

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,23 @@
 "use strict";
 
 function convert() {
-  const text = document.getElementById('text').value;
+  console.log("converting");
+  const text = document.getElementById("text").value;
   if (!text) return;
-  const lines = text.split('\n');
-  const canvas = document.getElementById('myCanvas');
-  const context = canvas.getContext('2d');
+  const lines = text.split("\n");
+  const canvas = document.getElementById("myCanvas");
+  const context = canvas.getContext("2d");
+  const width = Math.max(...lines.map(line => context.measureText(line).width));
+  console.log(width)
+  canvas.width = width + 20;
+  canvas.height = lines.length * 30 + 10;
 
-  context.fillStyle = '#cbd5e0';
+  context.fillStyle = "#cbd5e0";
   context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.font = 'italic 20pt Calibri';
-  context.textBaseline = 'middle';
-  context.fillStyle = 'black';
+  context.font = "italic 20pt Calibri";
+  context.textBaseline = "middle";
+  context.fillStyle = "black";
   for (var index = 0; index < lines.length; index++) {
-    context.fillText(lines[index], 10, 20+(index*30));
+    context.fillText(lines[index], 10, 20 + index * 30);
   }
 }
